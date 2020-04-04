5ffcb0e348eb6f99d0c00da792c8f36720385f88
mohammedAljadd <mohammedaljadd8@gmail.com>
2020-04-04 11:23:26 +0100

    login page

---
+++ code/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Login</title>
+</head>
+<body>
+
+    <img src="img/login.png" height="170" width="640" id="img" >
+
+    <div class="box">
+        <h1>Login</h1>
+    
+    <form action="login_check.php" method="POST" >
+        <input type="text" placeholder="Email"  name="email" > 
+        <input type="password" placeholder="Password" name="password" > 
+        <span class="subset" > <input type="submit" name="submit" >   </span>      <span class="subset" > <input type="reset" value="reset"></span> 
+    </form>
+
+
+    </div>
+
+</body>
+</html>

8fa7cbca9de9adc79e5fdc1cc1771aa6497333ae
mohammedAljadd <mohammedaljadd8@gmail.com>
2020-04-09 18:29:55 +0100

    styling reservation of admin in affectation table

--- code/affectation.php
+++ code/affectation.php
@@ -55,8 +55,8 @@
         while($row = mysqli_fetch_assoc($result)){
             if($row['a']=="Mohammed AL JADD"){
                 echo "<tr>
-                <th style='color:rgb(255, 104, 104)'>C'est vous</th>  <th>".$row['b']."</th>
-                <th>".$row['c']."</th>  <th>".$row['d']."</th>
+                <th style='color:rgb(255, 104, 104)'>C'est vous</th>  <th style='color:rgb(255, 104, 104)'>".$row['b']."</th>
+                <th style='color:rgb(255, 104, 104)'>".$row['c']."</th>  <th style='color:rgb(255, 104, 104)'>".$row['d']."</th>
                 </tr>";
 
             }

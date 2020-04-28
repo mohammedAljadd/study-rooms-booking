d2814ec2c4a7480e0a7524fbdddec55eedb32861
mohammedAljadd <mohammedaljadd8@gmail.com>
2020-04-28 20:39:28 +0100

    add subject to sent message

--- code/contact_check.php
+++ code/contact_check.php
@@ -3,9 +3,9 @@ if($_POST['submit']){
     session_start();
     $name=$_POST['smya'];
     $email=$_SESSION['email'];
-    $subject=$_POST['subject'];
+    $Subject=$_POST['subject'];
     $txt=$_POST['message'];
-    if(empty($name) || empty($subject) || empty($txt)){
+    if(empty($name) || empty($Subject) || empty($txt)){
         $_SESSION['contact'] = 'Champs vides';
         header("Location:contact.php");
     }
@@ -14,11 +14,11 @@ if($_POST['submit']){
         $sender = $email;
         $recipient = 'webaljadd@gmail.com';
 
-        $subject = "Un message de ".$name." envoyé depuis la page de réservation";
+        $subject = 'Reservation_contact :'.$Subject;
 
         $headers = 'From:' . $sender;
        
-        $message = "Vous avez reçu un message de ".$sender."\n\n".$txt;
+        $message = "Vous avez reçu un message de ".$name.' '.$sender."\n\n".$txt;
         if(mail($recipient, $subject, $message, $headers)){
             $_SESSION['contact'] = 'Courrier envoyé avec succès, Nous vous répondrons dès que possible!';
             header("Location:contact.php");
